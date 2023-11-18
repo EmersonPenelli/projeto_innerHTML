672f6639bd4d231806d5e1690ef42753e430c4ec
EmersonPenelli <emersonpenelli@gmail.com>
2023-11-18 16:01:50 -0300

    projeto innerHTML JavaScript

---
+++ main.js
@@ -0,0 +1,20 @@
+let h1 = document.getElementById('titulo');
+h1.innerText = "Titulo JavaScript"
+
+let ul = document.querySelector('ul');
+ul.innerHTML = `
+<li>item 01</li>
+<li>item 02</li>
+<li>item 03</li>
+`
+
+let proz = document.querySelector('a');
+proz.innerText = 'Link Proz'
+
+let ol = document.getElementById('lista-ordenada');
+ol.innerHTML = `
+<li><a href="https://www.google.com.br" target=_blank>Link 01</a></li>
+<li><a href="https://www.youtube.com.br" target=_blank>Link 02</a></li>
+<li><a href="https://www.pirituba.net/portal/" target=_blank>Link 03</a></li>
+`
+
